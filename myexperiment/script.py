5331c4f053bca512ed889ce40e54a91639bc370a
George_Plover <19307130237@fudan.edu.cn>
2023-05-04 15:55:21 +0800

    draw&compare

--- myexperiment/script.py
+++ myexperiment/script.py
@@ -1,5 +1,6 @@
 
 import itertools
+import matplotlib.pyplot as plt
 
 DataSets = ["lognormal-190M","longitudes-200M","longlat-200M","ycsb-200M"]
 
@@ -51,6 +52,34 @@ def Get_Record_from_File(filename, id, core_name):
                   insert_frac, throughput, memory, 
                   num_model_nodes, num_data_nodes, max_level) 
 
+# 画图，根据数据集，对不同的插入比例
+def Draw(Records: list , dataset: str, bulknum: int, insfrac: list, outfile = "chart.png"):
+    alex = []
+    my = []
+    for record in Records:
+        if record.dataset == dataset and record.num_bulk == bulknum:
+            if record.core_name == "alex":
+                alex.append(record.throughput)
+            else:
+                my.append(record.throughput)
+    
+    x = range(len(insfrac))
+
+    # 绘制条形图
+    plt.clf()
+    plt.bar(x, alex, width=0.4, label='alex')
+    plt.bar([i + 0.4 for i in x], my, width=0.4, label='my')
+
+    # 设置 x 轴刻度
+    plt.xticks([i + 0.2 for i in x], insfrac)
+
+    # 添加图例
+    plt.legend()
+
+    # 显示图形
+    plt.savefig(outfile)
+    
+
 file_path = "/home/nsh/ALEX/myexperiment/"
 CoreNames = ["my", "alex"]
 BulkNum = ["5e6", "1e7", "2e7"]
@@ -64,5 +93,9 @@ for corename,dataset,bulknum,insfrac in itertools.product(CoreNames, DataSets, B
     Records.append(
         Get_Record_from_File(path, id, corename)
     )
-    print(Records[-1],"\n")
+    # print(Records[-1],"\n")
 
+for dataset in DataSets:
+    for num in BulkNum:
+        bnum = int(float(num))
+        Draw(Records,dataset,bnum,InsertFrac,dataset+"&"+str(bnum)+"chart.png")
