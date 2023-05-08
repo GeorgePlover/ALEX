d12a3adfebca1985e48ed7c67cfdbcd8b7585d98
George_Plover <19307130237@fudan.edu.cn>
2023-05-08 21:39:54 +0800

    update my-results *core: my *keytype ULL

--- myexperiment/single_run.sh
+++ myexperiment/single_run.sh
@@ -6,11 +6,15 @@
 # ycsb-200M
 # binom-200M
 
-./run.sh longitudes-200M 200000000 5e6
-./run.sh longitudes-200M 200000000 1e7
-./run.sh longitudes-200M 200000000 2e7
+./run.sh ycsb-200M 200000000 5e6
+./run.sh ycsb-200M 200000000 1e7
+./run.sh ycsb-200M 200000000 2e7
 
-./run.sh longlat-200M 200000000 5e6
-./run.sh longlat-200M 200000000 1e7
-./run.sh longlat-200M 200000000 2e7
+./run.sh lognormal-190M 190000000 5e6
+./run.sh lognormal-190M 190000000 1e7
+./run.sh lognormal-190M 190000000 2e7
+
+./run.sh binom-200M 200000000 5e6
+./run.sh binom-200M 200000000 1e7
+./run.sh binom-200M 200000000 2e7
 
